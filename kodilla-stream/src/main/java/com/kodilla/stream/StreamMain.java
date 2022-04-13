a124bfc4b0ec30b6502afd879dc4cdd5a4091551
PLecicki <p.lecicki@gmail.com>
2022-04-13 10:50:46 +0200

    7.3 stream(), filter(), map() and collect() [corrected]

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,6 +3,7 @@ package com.kodilla.stream;
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
 
+import java.time.LocalDate;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -13,17 +14,18 @@ public class StreamMain {
         Forum theForum = new Forum();
         Map<Integer, ForumUser> theResultMapOfUsers = theForum.getUserList().stream()
                 .filter(forumUser -> forumUser.getSex() == 'M')
-                .filter(forumUser -> (forumUser.getDateOfBirth().getYear() < 2002) ||
-                        ((forumUser.getDateOfBirth().getYear() == 2002) &&
-                                (forumUser.getDateOfBirth().getMonthValue() < 4)) ||
-                        ((forumUser.getDateOfBirth().getYear() == 2002) &&
-                                (forumUser.getDateOfBirth().getMonthValue() == 4) &&
-                                (forumUser.getDateOfBirth().getDayOfMonth() <= 13)))
+                .filter(forumUser -> (forumUser.getDateOfBirth().getYear() < LocalDate.now().getYear()) ||
+                        ((forumUser.getDateOfBirth().getYear() == LocalDate.now().getYear()) &&
+                                (forumUser.getDateOfBirth().getMonthValue() < LocalDate.now().getDayOfMonth())) ||
+                        ((forumUser.getDateOfBirth().getYear() == LocalDate.now().getYear()) &&
+                                (forumUser.getDateOfBirth().getMonthValue() == LocalDate.now().getMonthValue()) &&
+                                (forumUser.getDateOfBirth().getDayOfMonth() <= LocalDate.now().getDayOfMonth())))
                 .filter(forumUser -> forumUser.getPostsCount() >= 1)
                 .collect(Collectors.toMap(ForumUser::getUserID, forumUser -> forumUser));
 
         theResultMapOfUsers.entrySet().stream()
                 .map(entry -> entry.getKey() + " - " + entry.getValue())
                 .forEach(System.out::println);
+
     }
 }
