9b1a05eb19a980b9ec897ebed715813cb88a4aa6
PLecicki <p.lecicki@gmail.com>
2022-06-22 23:26:23 +0200

    17.2 Create entities

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,34 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class TaskListDaoTestSuite {
+
+    @Autowired
+    private TaskListDao taskListDao;
+
+    @Test
+    void testFindByListName() {
+        //Given
+        TaskList taskList = new TaskList("ToDo", "Tasks to do in the future");
+        taskListDao.save(taskList);
+        String listName = taskList.getListName();
+
+        //When
+        List<TaskList> readTasks = taskListDao.findByListName(listName);
+
+        //Then
+        Assertions.assertEquals(1, readTasks.size());
+
+        //CleanUp
+        int id = readTasks.get(readTasks.size()-1).getId();
+        taskListDao.deleteById(id);
+    }
+}
